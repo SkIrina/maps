677b522f579e882ca8bef630fb045f030966c516
SkIrina <skudnovaira@gmail.com>
2024-12-16 17:15:25 +0900

    update deploy for vercel

--- dist/_nuxt/1b21283.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{122:function(n,o,w){w(123),n.exports=w(124)}},[[122,3,1,4]]]);
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,6 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
-  router: {
-    base: "/maps/", // Replace <repo-name> with your GitHub repository name
-  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
