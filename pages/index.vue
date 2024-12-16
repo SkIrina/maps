ace2293953ffcae0fb5f739b41ccb42b7c6e0b41
SkIrina <skudnovaira@gmail.com>
2024-12-16 18:29:38 +0900

    add clicks count

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
     <div>
         <button @click="moveToNavigation">ナビ</button>
         <span>https://www.google.co.jp/maps/dir/?api=1&destination=35.487676,133.049012&travelmode=driving</span>
+        <p @click="increment">クリック: {{ count }}</p>
     </div>
 </template>
 
@@ -9,14 +10,24 @@
 export default {
     data() {
         return {
-            mountains: []
+            count: 0
         }
     },
     methods: {
         moveToNavigation() {
+            this.increment()
             const fallbackUrl = `https://www.google.co.jp/maps/dir/?api=1&destination=35.487676,133.049012&travelmode=driving`;
             window.location.href = fallbackUrl;
+        },
+        increment() {
+            this.count++
         }
     }
 }
 </script>
+
+<style>
+button {
+    padding-inline: 20px;
+}
+</style>
